5bac3815d86111ccfb084ed15c78b3e468658ef2
Kaveesha2000 <kaveesha200066@gmail.com>
2021-12-19 19:57:27 +0530

    updating

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class StudentFormController {
+    public Button btnSave;
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void moveToHome(MouseEvent mouseEvent) {
+    }
 }
