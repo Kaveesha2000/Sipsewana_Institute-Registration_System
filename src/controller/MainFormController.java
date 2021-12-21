81720a95a8ea7dbc3d25f9f2ec4e328aa4c09c90
Kaveesha2000 <kaveesha200066@gmail.com>
2021-12-21 17:54:36 +0530

    finishing dash board form

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class MainFormController {
+    public AnchorPane mainFormContext;
+
+    public void logInOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/DashBoardForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) mainFormContext.getScene().getWindow();
+        window.setScene(new Scene(load));
+    }
 }
