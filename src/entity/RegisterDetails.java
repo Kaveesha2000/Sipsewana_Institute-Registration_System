228915f4e03f3758c0627a552a1bde75bc09e61d
Kaveesha2000 <kaveesha200066@gmail.com>
2021-12-26 12:39:23 +0530

    finishing registerDetails entity

--- src/entity/RegisterDetails.java
+++ src/entity/RegisterDetails.java
@@ -1,4 +1,51 @@
 package entity;
 
+import java.time.LocalDate;
+
 public class RegisterDetails {
+    private String SId;
+    private String CId;
+    private LocalDate RegDate;
+
+    public RegisterDetails() {
+    }
+
+    public RegisterDetails(String SId, String CId, LocalDate regDate) {
+        this.setSId(SId);
+        this.setCId(CId);
+        setRegDate(regDate);
+    }
+
+    public String getSId() {
+        return SId;
+    }
+
+    public void setSId(String SId) {
+        this.SId = SId;
+    }
+
+    public String getCId() {
+        return CId;
+    }
+
+    public void setCId(String CId) {
+        this.CId = CId;
+    }
+
+    public LocalDate getRegDate() {
+        return RegDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        RegDate = regDate;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterDetails{" +
+                "SId='" + SId + '\'' +
+                ", CId='" + CId + '\'' +
+                ", RegDate=" + RegDate +
+                '}';
+    }
 }
