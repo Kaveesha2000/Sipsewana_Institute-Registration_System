0de3a83a8bfa221676ee41b7a8000a4729ea960e
Kaveesha2000 <kaveesha200066@gmail.com>
2021-12-26 12:52:38 +0530

    adding contents

---
+++ src/controller/RegisterDetailsFormController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class RegisterDetailsFormController {
+    public AnchorPane registrationDetailContext;
+
+    public void moveToHome(MouseEvent mouseEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+}
